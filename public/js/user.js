02dc6188312705af8239358c45b0c6c09a138fa0
Christian Farmer <christian.farmer@paymentspring.com>
2023-03-26 16:28:02 -0500

    Add comment

--- public/js/user.js
+++ public/js/user.js
@@ -2,6 +2,19 @@
 var output = document.querySelector("#output")
 
 async function getUser() {
+  /*
+    NOTE: This is fetching User 1 every time, but you can
+    add an input on the page and do the following:
+
+    ```
+      var idInput = document.querySelector("#id-input")
+      var id = idInput.value
+    ```
+
+    Then use this URL: `/api/user/${id}` in the fetch
+    request below
+  */
+
   var response = await fetch("/api/user/1", { method: "GET" })
   var user = await response.json()
 
